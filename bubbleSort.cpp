deda31f586476d0e137f0d0ef1c2ecc4ead3db9a
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-05 10:53:50 +0700

    output ke layar

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -6,5 +6,7 @@ int arr[20]; //deklarasi variabel global array a dengan ukuran 20
 int n; //deklarasi variabel global n untuk menyimpan banyakya elemen
 
 void input(){ //prosedur untuk input
-    
+    while (true){
+        cout << "Masukkan banyaknya elemen pada array : "; //output ke layar
+    }
 }
