77a79ee44ee636603bb3f87d59d2d97d6da6617a
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-05 11:36:17 +0700

    menampilkan array

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -55,6 +55,6 @@ void display(){
     cout << "================================" <<endl;
     cout << endl;
     for(int j = 0; j < n; j++){
-        
+        cout << arr[j]; //menampilkan array
     }
 }
