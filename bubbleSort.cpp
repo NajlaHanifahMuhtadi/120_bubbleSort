c0467042724d08ff0a5d3606c9cb7947a42dbcc3
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-05 11:50:19 +0700

    perbaikan 1 menjadi i

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -20,7 +20,7 @@ void input(){ //prosedur untuk input
     cout << "Masukkan lemen array" << endl;
     cout << "=========================" << endl;
 
-    for (int i=0; 1 < n; i++){
+    for (int i = 0; i < n; i++){
         cout << "Data ke- " << (i+1) << ": "; //output menampilkan data ke-
         cin >> arr[i]; //input dari pengguna
     }
@@ -41,7 +41,7 @@ void bubbleSortArray(){ //prosedur untuk mengurutkan array
         pass = pass + 1; //step 4
 
         cout << "\nPass " << pass - 1 << ": "; //number of pass
-            for (int k =0; k < n; k++){
+            for (int k = 0; k < n; k++){
             cout << arr[k] << " "; //menampilkan data pada number of pass
         } 
         cout << endl;
@@ -76,5 +76,5 @@ int main(){
 
     system("pause");
     return 0;
-    
+
 }
